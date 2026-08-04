1be4ab962f43ec3ec4ff73afcfda9f52f8f910ac
Kherlysson Ryann <ryann.gto@gmail.com>
2026-08-04 18:14:29 -0300

    chore: prepara aplicacao para deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,17 +9,31 @@ import supervisorRoutes from "./routes/supervisorRoutes.js";
 
 const app = express();
 
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "20kb",
+  }),
+);
 
 app.get("/", (request, response) => {
   return response.status(200).json({
     mensagem: "API do sistema de presença funcionando.",
+  });
+});
+
+app.get("/health", (request, response) => {
+  return response.status(200).json({
+    status: "ok",
   });
 });
 
